bf36134da578a1e5e0b8d6d7fe06e771871f0117
Dom Montalto <dmontalto@imperosoftware.com>
2025-03-22 06:41:49 +0000

    Initial Commit

---
+++ SwiftDataExpenses/SwiftDataExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExpensesApp.swift
+//  SwiftDataExpenses
+//
+//  Created by Dominic Montalto on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
